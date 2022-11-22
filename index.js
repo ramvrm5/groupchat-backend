b48d37ae64d31aa8096e15a1c9c5cf38bee289ab
ramvrm <ramverma@synergytechs.net>
2022-11-22 17:03:15 +0530

    make it better

--- index.js
+++ index.js
@@ -21,9 +21,9 @@ const io = new Server(server);
 
 
 
-/* app.get('/', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
-}); */
+});
 
 io.on('connection', (socket) => {
     console.log('a user connected');
