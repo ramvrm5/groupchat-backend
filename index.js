24201eeac7d93b48e759b7a712362425c532e520
ramvrm <ramverma@synergytechs.net>
2022-11-22 20:57:03 +0530

    chnages for socket.io intially done

--- index.js
+++ index.js
@@ -6,20 +6,34 @@ const app = express();
 app.use(cors());
 const http = require('http');
 const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
+const io = require("socket.io")(server, {
+    cors: {
+        origin: "vscode-webview://11puh1rgk911fukq9ade1hsgt1rsabn3f3cfqo4schec3a1nkahv",
+        methods: ["GET", "POST"]
+    }
+});
+//const io = new Server(server);
 
 
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
-
+let users = [];
 io.on('connection', (socket) => {
-    console.log('a user connected');
-    socket.on('chat message', msg => {
-        console.log('From client: ', msg)
-        io.emit('chat message', msg)
+    console.log('A user connected');
+    socket.on('setUsername', function (data) {
+        console.log(data);
+        if (users.indexOf(data.username) > -1) {
+            socket.emit('userExists', data.username + ' username is taken! Try some other username.');
+        } else {
+            users.push(data);
+            socket.emit('userSet', { username: data.username });
+        }
+    });
+    socket.on('msg', function (data) {
+        //Send message to everyone
+        io.sockets.emit('newmsg', data);
     })
 });
 
